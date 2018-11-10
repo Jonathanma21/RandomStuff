107192ad0bd919b84590ca302aa2c120c314e0b4
Jonathanma21 <42975675+Jonathanma21@users.noreply.github.com>
2018-11-09 23:40:32 -0500

    Add files via upload

---
+++ src/ifIntro.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ * By gay
+ *If Birthday Assignment
+ */
+
+import java.util.Scanner;
+public class ifIntro 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner user = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		String name = user.nextLine();
+		System.out.print("\nPlease enter your birthday: ");
+		int birthDay = user.nextInt();
+		System.out.print("\nPlease enter your birthmonth (number): ");
+		int birthMonth = user.nextInt();
+		System.out.print("\nPlease enter your birthyear: ");
+		int birthYear = user.nextInt();
+		
+		System.out.print("\nNow for the second person. Please press enter...");
+		user.nextLine();
+		System.out.print("\nSecond person, please enter your name: ");
+		String name2 = user.nextLine();
+		System.out.print("\nPlease enter your birthday: ");
+		int birthDay2 = user.nextInt();
+		System.out.print("\nPlease enter your birthmonth (number): ");
+		int birthMonth2 = user.nextInt();
+		System.out.print("\nPlease enter your birthyear: ");
+		int birthYear2 = user.nextInt();
+		
+		if (birthYear < birthYear2)
+		{
+			System.out.println(name + " is older then " + name2);
+			if (birthYear == birthYear2)
+			{	
+				if (birthMonth < birthMonth2)
+				{
+					System.out.println(name + " is older then " + name2);
+				
+					if (birthMonth == birthMonth2)
+					{
+						if (birthDay < birthDay2)
+						{
+							System.out.println(name + " is older then " + name2);
+						}
+						else
+						{
+							System.out.println(name2 + " is older then " + name);
+						}
+					}
+				}
+				else
+				{	
+					System.out.println(name2 + " is older then " + name);
+				}
+			}
+		
+		
+		}
+		else
+		{
+			System.out.println(name2 + " is older then " + name);
+		}
+		
+		}
+	}
